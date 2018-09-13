8ae9935cfd249fe41c66b9c88e7435c929ae4b6a
Lars Bohm <larsbohm@gmail.com>
2018-09-13 18:07:43 +0200

    set up pagination, basic bootstrap, simple layout and category options yml

--- app/controllers/mushrooms_controller.rb
+++ app/controllers/mushrooms_controller.rb
@@ -1,13 +1,13 @@
 class MushroomsController < ApplicationController
   def index
-    @mushrooms = Mushroom.all
+    @mushrooms = Mushroom.order(id: :asc).paginate(page: params[:page], per_page: 100)
     @mushroom_attributes = mushroom_attributes
   end
 
   private
 
   def mushroom_attributes
-    Mushroom.new.attributes.keys
+    Mushroom.new.attributes.keys - %w(created_at updated_at)
   end
 
 end
